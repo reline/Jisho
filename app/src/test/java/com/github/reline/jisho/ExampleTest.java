88c4c5bb50be78d6c91439800fc1f9e1a27bd76e
Nathan Reline <nathanielreline@gmail.com>
2017-05-11 18:34:17 -0700

    Moved the dictionary database module into this project

---
+++ app/src/test/java/com/github/reline/jisho/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.github.reline.jisho;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertTrue;
+
+public class ExampleTest {
+
+    @Test
+    public void test() throws Exception {
+        assertTrue(true);
+    }
+
+}
---
+++ jishodb/src/main/java/io/github/reline/jishodb/MainActivity.kt
@@ -0,0 +1,12 @@
+package io.github.reline.jishodb
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        finish()
+    }
+}
