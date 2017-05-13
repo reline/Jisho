852769e1207ea29fe677bdcb6a268acf4a3174c1
Nathan Reline <nathanielreline@gmail.com>
2017-05-13 06:18:33 -0700

    Update copyright

--- jishodb/src/main/java/io/github/reline/jishodb/dbmodels/Link.kt
+++ jishodb/src/main/java/io/github/reline/jishodb/dbmodels/Link.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016 Nathaniel Reline
+ * Copyright 2017 Nathaniel Reline
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
