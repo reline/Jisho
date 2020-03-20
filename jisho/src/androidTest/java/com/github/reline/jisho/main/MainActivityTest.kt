27419b76041bd76a889e6c274eae29439fdb8392
Nathan <nathanielreline@gmail.com>
2020-03-19 19:53:47 -0700

    Espresso smoke test

---
+++ jisho/src/androidTest/java/com/github/reline/jisho/main/MainActivityTest.kt
@@ -0,0 +1,25 @@
+package com.github.reline.jisho.main
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import com.github.reline.jisho.R
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+    @get:Rule
+    val activityRule = ActivityTestRule(MainActivity::class.java)
+
+    @Test
+    fun smokeTest() {
+        onView(withId(R.id.mainActivityLogoTextView)).check(matches(isDisplayed()))
+    }
+}
