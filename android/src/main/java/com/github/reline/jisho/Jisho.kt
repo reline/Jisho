ba0aa7298e6fd0d359d3f1549c9bb71fa3587d59
Nathan Reline <nathan.reline@gm.com>
2020-03-25 21:18:30 -0700

    Multiplatformmm but Android only

---
+++ android/src/main/java/com/github/reline/jisho/Jisho.kt
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2017 Nathaniel Reline
+ *
+ * This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
+ * To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or
+ * send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ */
+
+package com.github.reline.jisho
+
+import android.app.Application
+import com.github.reline.jisho.injection.components.ApplicationComponent
+import com.github.reline.jisho.injection.components.DaggerApplicationComponent
+import timber.log.Timber
+import javax.inject.Inject
+
+class Jisho : Application() {
+
+    lateinit var appComponent: ApplicationComponent
+
+    @Inject
+    lateinit var tree: Timber.Tree
+
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerApplicationComponent.builder()
+                .application(this)
+                .build()
+        appComponent.inject(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        } else {
+            Timber.plant(tree)
+        }
+    }
+}
---
+++ ios/Jisho/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Jisho
+//
+//  Created by Nathan Reline on 3/25/20.
+//  Copyright © 2020 Nathan Reline. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
