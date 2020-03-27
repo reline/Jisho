bf23ca3fc16e7de91eac22c84df947dc007c4c47
Nathan Reline <nathanielreline@gmail.com>
2020-03-26 19:34:13 -0700

    Multiplatform

---
+++ common/src/commonMain/kotlin/com/github/reline/jisho/sql/persistence/JapaneseMultilingualDao.kt
@@ -0,0 +1,6 @@
+package com.github.reline.jisho.sql.persistence
+
+import com.github.reline.jisho.sql.JishoDatabase
+
+class JapaneseMultilingualDao(private val database: JishoDatabase) {
+}
---
+++ ios/Jisho/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Jisho
+//
+//  Created by Nathan Reline on 3/25/20.
+//  Copyright © 2020 Nathan Reline. All rights reserved.
+//
+
+import SwiftUI
+import common
+
+struct ContentView: View {
+    var body: some View {
+        Text(ConstantsKt.JISHO_DB)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
