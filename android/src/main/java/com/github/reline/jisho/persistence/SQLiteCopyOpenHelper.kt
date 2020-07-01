d6a1a0a13c01e7943c3d226989bc1c7eb5a31764
Nathan Reline <nathanielreline@gmail.com>
2020-06-30 20:53:03 -0700

    Fix up copyright on Google and AOSP source code

--- android/src/main/java/com/github/reline/jisho/persistence/SQLiteCopyOpenHelper.kt
+++ android/src/main/java/com/github/reline/jisho/persistence/SQLiteCopyOpenHelper.kt
@@ -1,9 +1,17 @@
 /*
- * Copyright 2020 Nathaniel Reline
+ * Copyright 2019 The Android Open Source Project
  *
- * This work is licensed under the Creative Commons Attribution-ShareAlike 4.0 International License.
- * To view a copy of this license, visit http://creativecommons.org/licenses/by-sa/4.0/ or
- * send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.github.reline.jisho.persistence
