68486fe5a8f7413bc724039a81cb930f875b1f6a
Nathan Reline <nathanielreline@gmail.com>
2023-11-14 01:39:40 -0700

    Migrate remaining prepopulator files

--- database/src/main/kotlin/com/github/reline/jisho/dictmodels/jmdict/Dictionary.kt
+++ database/src/main/kotlin/com/github/reline/jisho/dictmodels/jmdict/Dictionary.kt
@@ -11,6 +11,14 @@ package com.github.reline.jisho.dictmodels.jmdict
 import com.tickaroo.tikxml.annotation.Element
 import com.tickaroo.tikxml.annotation.Xml
 
+/**
+ * +	a+	One or more occurrences of a
+ * *	a*	Zero or more occurrences of a
+ * ?	a?	Either a or nothing
+ * ,	a, b	a followed by b
+ * |	a | b	a followed by b
+ * ()	(expression)	An expression surrounded by parentheses is treated as a unit and could have any one of the following suffixes ?, *, or +.
+ */
 @Xml(name = "Dictionary")
 open class Dictionary {
 
