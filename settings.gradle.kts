38bd5e2b9faf0182a1ddc9e5cd5032b05e63e09b
Nathan Reline <nathanielreline@gmail.com>
2024-03-15 20:40:09 -0700

    Include 'android' as a subproject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,66 @@
-includeBuild("database")
-includeBuild("prepopulator")
-includeBuild("android")
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+
+        exclusiveContent {
+            forRepository {
+                maven {
+                    url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+                }
+            }
+            filter {
+                includeGroup("com.tickaroo.tikxml")
+            }
+        }
+
+        exclusiveContent {
+            forRepository {
+                maven {
+                    url = uri("https://www.atilika.org/nexus/content/repositories/atilika")
+                }
+            }
+            filter {
+                includeGroup("org.atilika.kuromoji")
+            }
+        }
+
+        includeBuild("prepopulator")
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+
+        exclusiveContent {
+            forRepository {
+                maven {
+                    url = uri("https://www.jitpack.io")
+                }
+            }
+            filter {
+                includeGroup("com.github.reline")
+            }
+        }
+
+        exclusiveContent {
+            forRepository {
+                maven {
+                    url = uri("https://www.atilika.org/nexus/content/repositories/atilika")
+                }
+            }
+            filter {
+                includeGroup("org.atilika.kuromoji")
+            }
+        }
+
+        includeBuild("database")
+    }
+}
+
+include("android")
 
 rootProject.name = "Jisho"
