ed019939f49f398c2a6fac11f83affa4a8e1a1ca
Nathan Reline <nathanielreline@gmail.com>
2024-04-17 15:09:38 -0700

    Run all tests in CI/CD

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,9 @@
 tasks.register("clean", Delete::class) {
     rootProject.layout.buildDirectory.get().asFile.delete()
 }
+
+// todo: forward cli params
+tasks.named("dependencies").configure {
+    dependsOn(subprojects.map { it.tasks.dependencies })
+    dependsOn(gradle.includedBuilds.map { it.task(":dependencies") })
+}
